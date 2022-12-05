1c7a4d2aa883e1d2135073adfc2ef794ae108074
tzu huang yen <912yan@gmail.com>
2022-12-06 00:03:06 +0100

    cartList

--- js/all.js
+++ js/all.js
@@ -23,8 +23,9 @@ function getProductData() {
       console.log(error);
     });
 }
-//2.渲染畫面
+
 const allProducts = document.querySelector(".allProducts");
+//重組字串 可重複使用
 function combineProductHTMLItem(item) {
   return ` <li class="productCard">
 <h4 class="productType">新品</h4>
@@ -32,18 +33,19 @@ function combineProductHTMLItem(item) {
   src="${item.images}"
   alt=""
 />
-<a href="#" class="addCardBtn js-addCart" data-id="${item.id}">加入購物車</a>
+<a href="#"  class="js-addCart" data-id="${item.id}">加入購物車</a>
 <h3>${item.title}</h3>
 <del class="originPrice">NT$${item.origin_price}</del>
 <p class="nowPrice">NT$${item.price}</p>
 </li>`;
 }
+//2.渲染畫面
 function renderProduct(data) {
   let str = "";
   data.forEach((item) => {
     str += combineProductHTMLItem(item);
   });
-  allProducts.setAttribute("selected", "selected");
+
   productWrap.innerHTML = str;
   searchResultText.innerHTML = `本次搜尋共 ${data.length}筆資料`;
 }
@@ -58,24 +60,82 @@ productSelect.addEventListener("change", (e) => {
     let targetData = [];
     productData.forEach((item) => {
       if (item.category === currValue) targetData.push(item);
+      allProducts.setAttribute("selected", "selected");
       renderProduct(targetData);
     });
   }
   // 放暫存資料的變數，用來放被篩選過的資料
 });
+
 //3.取得購物車列表
-// let cartData = [];
-// function getCartData() {
-//   axios
-//     .get(
-//       `https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/carts`
-//     )
-//     .then(function (res) {
-//       cartData = res.data.products;
-//       console.log(res.data.products);
-//     })
-//     .catch(function (error) {
-//       console.log(error);
-//     });
-// }
-// getCartData();
+const shoppingCartTableList = document.querySelector(".shoppingCart-tableList");
+let cartData = [];
+function getCartList() {
+  axios
+    .get(
+      `https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/carts`
+    )
+    .then(function (res) {
+      cartData = res.data.carts;
+      //console.log(cartData);
+      let str = "";
+      cartData.forEach((item) => {
+        str += `<tr>
+        <td>
+          <div class="cardItem-title">
+            <img src="${item.product.images}" alt="" />
+            <p>${item.product.title}</p>
+          </div>
+        </td>
+        <td>NT$${item.product.price}</td>
+        <td>${item.quantity}</td>
+        <td>NT$${item.product.price * item.quantity}</td>
+        <td class="discardBtn">
+          <a href="#" class="material-icons"> clear </a>
+        </td>
+      </tr>`;
+      });
+      shoppingCartTableList.innerHTML = str;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+getCartList();
+//3-2 加入購物車按鈕 addEventListener
+productWrap.addEventListener("click", (e) => {
+  e.preventDefault();
+  //console.log(e.target);
+  let addCartClass = e.target.getAttribute("class");
+  //console.log(addCartClass);
+  if (addCartClass !== "js-addCart") {
+    return;
+  }
+  let productId = e.target.getAttribute("data-id");
+  //console.log(productId);
+
+  //預設數量是１
+  let quantityNum = 1;
+  //確認購物車內有無重複的品項
+  cartData.forEach((item) => {
+    if (item.product.id === productId) {
+      quantityNum = item.quantity += 1;
+    }
+  });
+  //console.log(quantityNum);
+  axios
+    .post(
+      `https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/carts`,
+      {
+        data: {
+          productId: productId,
+          quantity: quantityNum,
+        },
+      }
+    )
+    .then((res) => {
+      console.log(res);
+      alert("加入購物車");
+      getCartList();
+    });
+});
